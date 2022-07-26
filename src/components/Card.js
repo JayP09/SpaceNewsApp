ef067bd9f12453ad759f365d6e9513628954a9ea
Jay Panchal <panchalj272@gmail.com>
2022-07-27 01:11:19 +0530

    final commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,8 @@ import React from "react"
 
 const Card = ({item}) => {
     return (
-        <div className="p-2 bg-[#e1d8ee]">
-            <div className="p-[2px] bg-[#1f1122] h-full">
+        <div className="flex justify-center flex-auto p-2 bg-[#e1d8ee]">
+            <div className="flex justify-center flex-auto p-[2px] bg-[#1f1122] h-full">
                 <div className="flex justify-center flex-auto p-2 w-96 bg-[#e1d8ee] text-[#1f1122] cursor-pointer hover:bg-[#e0c6b2] h-full" onClick={() => window.location.href = item.url}>
                     <div className="flex flex-col items-center">
                         <img src={item.imageUrl} alt="img"/>
