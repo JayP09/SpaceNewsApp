4831ed77a11598671b7f851ab3054ad885dddfd9
Jay Panchal <panchalj272@gmail.com>
2022-07-27 00:51:14 +0530

    feature: updated files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState,useEffect} from "react"
+import Card from "./components/Card"
 
 function App() {
+  const [newsList, setNewsList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.spaceflightnewsapi.net/v3/articles")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setNewsList(data)
+      })
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-4xl font-[arial] py-5 text-center bg-[#1f1122] bg-[url('/src/assets/download.svg')]">
+      <div className="py-5">
+        <h1 className="text-[#e1d8ee]">Space News</h1>
+      </div>
+      <div className="flex flex-wrap gap-4 p-5 rounded">
+        {newsList.map((item) => (
+          <Card item={item}/>
+        ))}
+      </div>
     </div>
   );
 }
